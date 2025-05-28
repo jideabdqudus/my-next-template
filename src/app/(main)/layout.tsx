f7474c6a45560dee1fb5af226cace813e42e0508
Jide <jideabdqudus@gmail.com>
2025-05-28 23:37:52 +0100

    fix: remove description and keywords

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,8 +4,7 @@ import { generateMetadata } from "@/src/lib/utils";
 
 export const metadata = generateMetadata({
   title: siteName,
-  keywords: ["DataTail", "Retail data"],
-  description: "Retail data analysis and insights.",
+  description: "My site description",
 });
 
 export default function MainLayout({ children }: { children: ReactNode }) {
